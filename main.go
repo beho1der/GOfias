84e54ac092d4484033e978a5edad208f1e940145
Dmitri Ponomarev <captain.perreiro@gmail.com>
2018-05-08 17:37:06 +0700

    It Work

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 )
 
-type Adress struct {
+type Address struct {
 	AOGUID     string `xml:"AOGUID,attr"`
 	FORMALNAME string `xml:"FORMALNAME,attr"`
 	OFFNAME    string `xml:"FOFFNAME,attr"`
@@ -23,12 +23,16 @@ type Adress struct {
 }
 
 type Data struct {
-	Doctypes []Doctype `xml:"NormativeDocumentType"`
+	Doctypes []Address `xml:"NormativeDocumentType"`
 }
 
 type Mongo_Region struct {
-	Region string `bson:"region"`
-	Code   string `bson:"regioncode"`
+	Region   string `bson:"region"`
+	Code     string `bson:"regioncode"`
+	Fullname string `bson:"fullname"`
+	Sufix    string `bson:"sufix"`
+	Parent   string `bson:"parent"`
+	Id       string `bson:"id"`
 }
 
 func main() {
@@ -39,7 +43,7 @@ func main() {
 	}
 	defer session.Close()
 	// получаем коллекцию
-	normaCollection := session.DB("fias").C("norma")
+	regionCollection := session.DB("fias").C("region")
 
 	xmlFile, err := os.Open("/home/captain/Загрузки/fias_xml/AS_ADDROBJ.XML")
 	if err != nil {
@@ -64,18 +68,20 @@ func main() {
 			inElement = se.Name.Local
 			// ...and its name is "page"
 			if inElement == "Object" {
-				var data Doctype
+				var data Address
 				// decode a whole chunk of following XML into the
 				// variable p which is a Page (se above)
 				decoder.DecodeElement(&data, &se)
-				fmt.Printf("%v", data)
-				p1 := &Cast{Ndtype: data.NDTYPEID, Normative: data.NAME}
-				err = normaCollection.Insert(p1)
-				if err != nil {
-					fmt.Println(err)
+				if data.CURRSTATUS == 0 && data.LIVESTATUS == 1 && data.AOLEVEL == 1 && data.REGIONCODE == "01" {
+					fmt.Printf("%v", data)
+					p1 := &Mongo_Region{Region: data.FORMALNAME, Code: data.REGIONCODE, Fullname: data.FORMALNAME, Sufix: data.SHORTNAME, Parent: data.PARENTGUID, Id: data.AOGUID}
+					err = regionCollection.Insert(p1)
+					if err != nil {
+						fmt.Println(err)
+					}
+					// Do some stuff with the page.
+					total++
 				}
-				// Do some stuff with the page.
-				total++
 			}
 		default:
 		}
