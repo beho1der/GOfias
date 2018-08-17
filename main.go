799fab5009baeb284493eaf212addcb2ab5505e3
captain.perreiro@gmail.com <captain.perreiro@gmail.com>
2018-08-17 16:43:17 +0700

    Первые доработки

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"encoding/xml"
 	"fmt"
 	"gopkg.in/mgo.v2"
-	//	"gopkg.in/mgo.v2/bson"
+	"io/ioutil"
 	"os"
+	"regexp"
+	"unsafe"
 )
+
+var m map[string][]string
 
 type Address struct {
 	AOGUID     string `xml:"AOGUID,attr"`
@@ -22,6 +26,14 @@ type Address struct {
 	AOLEVEL    int    `xml:"AOLEVEL,attr"`
 }
 
+type House struct {
+	AOGUID   string `xml:"AOGUID,attr"`
+	ENDDATE  string `xml:"ENDDATE,attr"`
+	HOUSENUM string `xml:"HOUSENUM,attr"`
+	BUILDNUM string `xml:"BUILDNUM,attr"`
+	STRUCNUM string `xml:"STRUCNUM,attr"`
+}
+
 type Data struct {
 	Doctypes []Address `xml:"NormativeDocumentType"`
 }
@@ -31,29 +43,121 @@ type Mongo_Region struct {
 	Code     string `bson:"regioncode"`
 	Fullname string `bson:"fullname"`
 	Sufix    string `bson:"sufix"`
-	Parent   string `bson:"parent"`
-	Id       string `bson:"id"`
+	Id       string `bson:"_id"`
 }
 
-func main() {
+type Mongo_City struct {
+	City     string `bson:"city"`
+	Fullname string `bson:"fullname"`
+	Sufix    string `bson:"sufix"`
+	Parent   string `bson:"parent"`
+	Id       string `bson:"_id"`
+}
 
-	session, err := mgo.Dial("mongodb://192.168.155.5")
+type Mongo_Street struct {
+	Street   string `bson:"street"`
+	Fullname string `bson:"fullname"`
+	Sufix    string `bson:"sufix"`
+	Parent   string `bson:"parent"`
+	Id       string `bson:"_id"`
+}
+
+type Mongo_House struct {
+	Houseid  string `bson:"houseid"`
+	Number   string `bson:"number"`
+	Strnum   string `bson:"strnum"`
+	Buildnum string `bson:"buildnum"`
+}
+
+// читает директорию и находит совпадения по имени,возвращает полное имя файла
+func readDir(patch, filter string) string {
+	re := regexp.MustCompile(filter)
+	files, err := ioutil.ReadDir(patch)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	defer session.Close()
-	// получаем коллекцию
-	regionCollection := session.DB("fias").C("region")
+	for _, f := range files {
+		t := re.FindStringSubmatch(f.Name())
+		if len(t) >= 1 {
+			return f.Name()
+		}
 
-	xmlFile, err := os.Open("/home/captain/Загрузки/fias_xml/AS_ADDROBJ.XML")
+	}
+	return "Отсутсвует файл"
+}
+
+// функция поиска в массиве нужного значение
+func findAO(s []int, f int) bool {
+	for i := range s {
+		if s[i] == f {
+			return true
+		}
+	}
+	return false
+}
+
+func createHouse(s *mgo.Session, v string) {
+	session := s.Copy()
+	defer session.Close()
+	streetCollection := session.DB("fias").C("street")
+	patch := "/home/captain/Загрузки/fias_xml/"
+	name := readDir(patch, "AS_HOUSE")
+	xmlFile, err := os.Open(patch + name)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer xmlFile.Close()
-
 	decoder := xml.NewDecoder(xmlFile)
-	total := 0
+	var inElement string
+	for {
+		// Read tokens from the XML document in a stream.
+		t, _ := decoder.Token()
+		if t == nil {
+			break
+		}
+		// Inspect the type of the token just read.
+		switch se := t.(type) {
+		case xml.StartElement:
+			// If we just read a StartElement token
+			inElement = se.Name.Local
+			// ...and its name is "page"
+			if inElement == "House" {
+				var data House
+				decoder.DecodeElement(&data, &se)
+				if data.CURRSTATUS == 0 && data.LIVESTATUS == 1 && findAO(cityCode, data.AOLEVEL) == true && data.REGIONCODE == v {
+					//fmt.Printf("%v", data)
+					p := &Mongo_City{City: data.FORMALNAME, Fullname: data.FORMALNAME, Sufix: data.SHORTNAME, Parent: data.PARENTGUID, Id: data.AOGUID}
+					err = cityCollection.Insert(p)
+					if err != nil {
+						fmt.Println(err)
+					}
+					city++
+				}
+			}
+		}
+	}
+}
+
+func createAddrobj(s *mgo.Session, v string) {
+	session := s.Copy()
+	defer session.Close()
+	regionCollection := session.DB("fias").C("region")
+	cityCollection := session.DB("fias").C("city")
+	streetCollection := session.DB("fias").C("street")
+	m = make(map[string][]string)
+	patch := "/home/captain/Загрузки/fias_xml/"
+	name := readDir(patch, "AS_ADDROBJ")
+	xmlFile, err := os.Open(patch + name)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer xmlFile.Close()
+	decoder := xml.NewDecoder(xmlFile)
+	var city, street, region = 0, 0, 0
+	cityCode := []int{4, 5, 6, 90}
+	streetCode := []int{7, 91}
 	var inElement string
 	for {
 		// Read tokens from the XML document in a stream.
@@ -69,26 +173,56 @@ func main() {
 			// ...and its name is "page"
 			if inElement == "Object" {
 				var data Address
-				// decode a whole chunk of following XML into the
-				// variable p which is a Page (se above)
+				// Декодируем xml и разбираем его в зависимости от полей
+				// AOLEVEL = 1 определяем регион
 				decoder.DecodeElement(&data, &se)
-				if data.CURRSTATUS == 0 && data.LIVESTATUS == 1 && data.AOLEVEL == 1 && data.REGIONCODE == "01" {
-					fmt.Printf("%v", data)
-					p1 := &Mongo_Region{Region: data.FORMALNAME, Code: data.REGIONCODE, Fullname: data.FORMALNAME, Sufix: data.SHORTNAME, Parent: data.PARENTGUID, Id: data.AOGUID}
-					err = regionCollection.Insert(p1)
+				// находим Регион
+				if data.CURRSTATUS == 0 && data.LIVESTATUS == 1 && data.AOLEVEL == 1 && data.REGIONCODE == v {
+					p := &Mongo_Region{Region: data.FORMALNAME, Code: data.REGIONCODE, Fullname: data.FORMALNAME, Sufix: data.SHORTNAME, Id: data.AOGUID}
+					err = regionCollection.Insert(p)
 					if err != nil {
 						fmt.Println(err)
 					}
-					// Do some stuff with the page.
-					total++
+					region++
+				}
+				// AOLEVEL = 4,5,6,90 определяем город,населенный пункт,СНТ
+				if data.CURRSTATUS == 0 && data.LIVESTATUS == 1 && findAO(cityCode, data.AOLEVEL) == true && data.REGIONCODE == v {
+					//fmt.Printf("%v", data)
+					p := &Mongo_City{City: data.FORMALNAME, Fullname: data.FORMALNAME, Sufix: data.SHORTNAME, Parent: data.PARENTGUID, Id: data.AOGUID}
+					err = cityCollection.Insert(p)
+					if err != nil {
+						fmt.Println(err)
+					}
+					city++
+				}
+				// находим Улицу
+				if data.CURRSTATUS == 0 && data.LIVESTATUS == 1 && findAO(streetCode, data.AOLEVEL) == true && data.REGIONCODE == v {
+					//fmt.Printf("%v", data)
+					p := &Mongo_Street{Street: data.FORMALNAME, Fullname: data.FORMALNAME, Sufix: data.SHORTNAME, Parent: data.PARENTGUID, Id: data.AOGUID}
+					err = streetCollection.Insert(p)
+					if err != nil {
+						fmt.Println(err)
+					}
+					// добавляем AOGUID в массив для поиска домов
+					m[v] = append(m[v], data.AOGUID)
+					street++
 				}
 			}
+
 		default:
 		}
-
 	}
+}
 
-	fmt.Printf("Total articles: %d \n", total)
-	//fmt.Printf("%v", data)
+func main() {
+	session, err := mgo.Dial("mongodb://192.168.155.5")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	createAddrobj(session, "01")
+	fmt.Println("Real size of m:", unsafe.Sizeof(m)+unsafe.Sizeof([1000]int32{}))
 
 }
+
+//fmt.Printf("Total articles: %d \n", total)
